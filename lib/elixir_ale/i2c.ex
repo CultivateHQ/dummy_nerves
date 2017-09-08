5fe11886ee21154b481dcd1326836249abb339f5
Paul Wilson <paulanthonywilson@gmail.com>
2017-09-08 16:16:49 -0700

    basic stubs for I2C

---
+++ lib/elixir_ale/i2c.ex
@@ -0,0 +1,15 @@
+defmodule ElixirALE.I2C do
+  @moduledoc """
+  Just providing stub methods for now.
+  Will make testable later.
+  """
+
+
+  def write(_, _) do
+    :ok
+  end
+
+  def write_read(_, _, _) do
+    :ok
+  end
+end
